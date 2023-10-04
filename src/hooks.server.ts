11d342be7455671e2a198c5a9716a70f5ab5227c
Leo Mendoza Jr <dev@skyturtle.io>
2023-10-04 12:16:50 -0700

    remove migrations in startup code

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,10 @@
 import { auth } from '$lib/server/lucia';
-import { runMigrations } from '$lib/server/migrate';
 import type { Handle } from '@sveltejs/kit';
 
 const startupCode = () => {
 	console.log('Running startup code');
 	// place code that you want to run on startup here
 	// e.g., migrations, db connections
-	runMigrations().catch((e) => {
-		console.error('Migration failed');
-		console.error(e);
-		process.exit(1);
-	});
 };
 
 startupCode();
